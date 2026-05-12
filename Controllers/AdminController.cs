14dd92d851accb9980bb25d8672ccd44c2ad9e2a
azharAltaweel <azharabdalmajeed@gmail.com>
2026-05-12 22:50:11 +0300

    Create admin users info page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using E_commerce_Website__Skincare_.Data;
+using E_commerce_Website__Skincare_.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce_Website__Skincare_.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+
+    public class AdminController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // USERS PAGE
+        public async Task<IActionResult> UsersInfo()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            return View(users);
+        }
+    }
+}
